fe9d6ee474d576cf542600bdfdecba5dc096d363
Anitha1256 <anitha3venu@gmail.com>
2024-07-15 11:54:53 +0530

    DOM Excercise

---
+++ javascriptDom.js
@@ -0,0 +1,9 @@
+function foo(){
+    var res=document.getElementById("Firstname").value;
+    var res1 = document.getElementById("Middlename").value;
+    var res2 = document.getElementById("Lastname").value;
+    var res3 =document.getElementById("email").value;
+    var res4 = document.getElementById("password").value;
+    console.log(res,res1,res2,res3,res4);
+
+}
